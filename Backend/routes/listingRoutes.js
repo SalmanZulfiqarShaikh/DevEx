197be6b7cca06bebb92047c9c931638ae36712d8
FasihDagia <fasihdagia2006@gmail.com>
2026-04-27 00:36:00 +0500

    minor changes in listing controller

--- Backend/routes/listingRoutes.js
+++ Backend/routes/listingRoutes.js
@@ -2,12 +2,12 @@ const express = require('express');
 const router  = express.Router();
 const { createListing, getAllListings, getOneListing, updateListing, deleteListing }
   = require('../contollers/Listing/listingController');
-const { isLoggedIn } = require('../middlewares/User/isLoggedin'); 
+const isLoggedin = require('../middlewares/User/isLoggedin'); 
 
 router.get('/',        getAllListings);
 router.get('/:id',     getOneListing);
-router.post('/create',       isLoggedIn, createListing);    
-router.put('/update/:id',     isLoggedIn, updateListing);    
-router.delete('/delete/:id',  isLoggedIn, deleteListing);    
+router.post('/create',        createListing);    
+router.put('/update/:id',      updateListing);    
+router.delete('/delete/:id',   deleteListing);    
 
 module.exports = router;
