99e936cd95cee0f44ca50f1c45cf0528ae9018b2
FasihDagia <fasihdagia2006@gmail.com>
2026-04-26 17:38:34 +0500

    Listing routes and controller

--- Backend/routes/listingRoutes.js
+++ Backend/routes/listingRoutes.js
@@ -6,8 +6,8 @@ const isLoggedin = require('../middlewares/User/isLoggedin');
 
 router.get('/',        getAllListings);
 router.get('/:id',     getOneListing);
-router.post('/',       isLoggedin, createListing);    
-router.put('/:id',     isLoggedin, updateListing);    
-router.delete('/:id',  isLoggedin, deleteListing);    
+router.post('/create',       isLoggedin, createListing);    
+router.put('/update/:id',     isLoggedin, updateListing);    
+router.delete('/delete/:id',  isLoggedin, deleteListing);    
 
 module.exports = router;
