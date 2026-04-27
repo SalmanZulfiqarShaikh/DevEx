0a00b989fcb01f53604003351a42af514da269a6
FasihDagia <fasihdagia2006@gmail.com>
2026-04-28 01:38:43 +0500

    implemented a middleware to listing routes

--- Backend/routes/listingRoutes.js
+++ Backend/routes/listingRoutes.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router  = express.Router();
 const { createListing, getAllListings, getOneListing, updateListing, deleteListing }
   = require('../contollers/Listing/listingController');
+const {isSeller} = require("../middlewares/User/isLoggedin")
 
 router.get('/',        getAllListings);
 router.get('/:id',     getOneListing);
-router.post('/create',        createListing);    
+router.post('/create', isSeller, createListing);    
 router.put('/update/:id',      updateListing);    
 router.delete('/delete/:id',   deleteListing);    
 
